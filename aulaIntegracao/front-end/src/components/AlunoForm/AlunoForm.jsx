167fad8ab0c16146ab95bd9b4631d8cdac789012
Joel Santos <i3975@rn.senac.local>
2025-06-16 17:16:27 -0300

    feat: finalização do crud

--- aulaIntegracao/front-end/src/components/AlunoForm/AlunoForm.jsx
+++ aulaIntegracao/front-end/src/components/AlunoForm/AlunoForm.jsx
@@ -1,16 +1,44 @@
 import { useEffect, useState } from "react"; // importas os hooks do react
 import AlunoService from "../../services/alunoService";
+import CursoService from "../../services/cursoService";
 
-function AlunoForm(){
-    const[matricula, setMatricula] = useState('');    
-    const[nome, setNome] = useState('');
-    const[cod_curso, setCod_Curso] = useState('');
+function AlunoForm({ alunoEditando, aoSalvar }) {
+    const [matricula, setMatricula] = useState('');
+    const [nome, setNome] = useState('');
+    const [cod_curso, setCod_Curso] = useState('');
+    const [cursos, setCursos] = useState([]);
+
+    //Função para carregar a lista de cursos 
+    const carregarCursos = async () => {
+        const lista = await CursoService.listar();
+        console.log(lista);
+        // Atualiza o estado (cursos) com a lista recebida, caso não receba um array é setado um array vazio para o estado.
+        setCursos(Array.isArray(lista) ? lista : []);
+    }
+    useEffect(() => {
+        if (alunoEditando) {
+            setMatricula(alunoEditando.matricula);
+            setNome(alunoEditando.nome);
+            setCod_Curso(alunoEditando.cod_curso);
+
+        }
+    }, [alunoEditando]);
+
+    useEffect(() => {
+        carregarCursos();
+    }, [])
 
     //Função para criar um Aluno
-    const handleSubmit = async(e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault(); //Evita o recarregamento da página
-        const data = await AlunoService.criar({matricula, nome, cod_curso})
-        console.log(data);
+        if (alunoEditando) {
+            const data = await AlunoService.atualizar(matricula, { nome, cod_curso })
+            console.log(data);
+        } else {
+            const data = await AlunoService.criar({ matricula, nome, cod_curso })
+            console.log(data);
+        }
+
         setMatricula(''); //Limpa o campo de matrícula
         setNome('');//Limpa o campo de nome
         setCod_Curso(''); //Limpa o campo de código do curso    
@@ -18,7 +46,7 @@ function AlunoForm(){
     };
     const matriculaPattern = "^[A-Z][0-9]{5}$";
     const codCursoPattern = "^[A-Z][0-9]{3}$";
-    return(
+    return (
         <>
             <form onSubmit={handleSubmit}>
                 <input
@@ -29,16 +57,17 @@ function AlunoForm(){
                     onChange={(e) => setMatricula(e.target.value)} //Atualiza o estado
                     maxLength={6}
                     minLength={6}
+                    disabled={!!alunoEditando}
                     required //campo obrigatorio
                 />
-                 <input
+                <input
                     type="text" // nome do Aluno
                     placeholder="Informe o nome do Aluno"
                     value={nome}//linkando a variavel de estado
                     onChange={(e) => setNome(e.target.value)} //Atualiza o estado
                     required //campo obrigatorio
                 />
-                <input
+                {/* <input
                     type="text" // Código do Curso
                     placeholder="Código do Curso (ex: A123)"
                     value={cod_curso}//linkando a variavel de estado
@@ -47,10 +76,26 @@ function AlunoForm(){
                     maxLength={4}
                     minLength={4}
                     required //campo obrigatorio
-                />
-                <button type="submit">Cadastrar</button>
+                />  */}
+                <select
+                    value={cod_curso}
+                    onChange={(e) => setCod_Curso(e.target.value)}
+                    required
+                >
+                    <option value="">Selecione um curso</option>
+                    {
+                        cursos.map((c) => (
+                            <option key={c.cod_curso} value={c.cod_curso}>
+                                {c.nome}
+                            </option>
+                        ))
+                    }
+                </select>
+
+
+                <button type="submit"> {alunoEditando ? 'Atualizar' : 'Cadastrar'} </button>
             </form>
-        
+
         </>
     )
 }
--- aulaIntegracao/front-end/src/components/ListarAlunos/ListarAlunos.jsx
+++ aulaIntegracao/front-end/src/components/ListarAlunos/ListarAlunos.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"; // importas os hooks do react
 import AlunoService from "../../services/alunoService";
 
-function ListarAlunos() {
+function ListarAlunos({aoEditar}) {
     //Estado para armazenar os Alunos
     const [alunos, setAlunos] = useState([]);
 
@@ -13,9 +13,25 @@ function ListarAlunos() {
         setAlunos(Array.isArray(lista) ? lista : []);
     }
 
+
+    //Função para deletar curso
+    const deletar = async (matricula, nome) => {
+        const confirm = window.confirm(`Deseja mesmo remover o aluno: ${nome} ?`);
+        if (confirm) {
+            const res = await AlunoService.deletar(matricula);
+            console.log(res)
+            if (res) {
+                alert('Aluno removido com sucesso!');
+                carregar();
+            } else {
+                alert('Erro ao remover!');
+            }
+        }
+    }
     //Executa a função carregar ao montar o componente ([])
     useEffect(() => {
         carregar();
+       
     }, []);
     return (
         <>
@@ -27,13 +43,14 @@ function ListarAlunos() {
                     )
                     :
                     (
-                        <table>
+                        <table border={1}>
                             <thead>
                                 <tr>
                                     <th>Matrícula</th>
                                     <th>Nome</th>
                                     <th>Código Curso</th>
                                     <th>Nome Curso</th>
+                                    <th>Ações</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -44,6 +61,10 @@ function ListarAlunos() {
                                             <td>{a.nome}</td>
                                             <td>{a.cod_curso}</td>
                                             <td>{a.Curso.nome}</td>
+                                            <td>
+                                                <button onClick={()=> aoEditar(a)}>Editar</button>
+                                                <button onClick={() => deletar(a.matricula, a.nome)}>Excluir</button>
+                                            </td>
                                         </tr>
                                     ))
                                 }
